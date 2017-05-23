1021e9f6a965f0adeca3d68701c663c4f68e4761
4lk4tr43 <4lk4tr43@gmail.com>
2017-05-23 23:40:49 +0200

    Formated styles

--- helpers/style.js
+++ helpers/style.js
@@ -1,25 +1,20 @@
 /**
  * Created by 4lk4t on 2017-05-17.
  */
-
 const chalk = require('chalk');
 
 module.exports.standard = function (msg) {
     return chalk.white(msg);
 };
-
+module.exports.error = function (msg) {
+    return chalk.red(msg);
+};
 module.exports.info = function (msg) {
     return chalk.blue(msg);
 };
-
 module.exports.success = function (msg) {
     return chalk.green(msg);
 };
-
 module.exports.warning = function (msg) {
     return chalk.yellow(msg);
-};
-
-module.exports.error = function (msg) {
-    return chalk.red(msg);
 };
