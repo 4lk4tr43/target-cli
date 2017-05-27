6313a16863133e5705ab9acc82527e533cca4e4f
4lk4tr43 <4lk4tr43@gmail.com>
2017-05-27 16:57:06 +0200

    Added offer command

---
+++ commands/offer.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 4lk4t on 2017-05-27.
+ */
+
+exports.run = function (args) {
+    switch (args[0]) {
+
+        default:
+            break;
+    }
+};
+
+exports.help = function () {
+    console.log(style.info('Manage and login into Adobe Service Accounts.'));
+};
