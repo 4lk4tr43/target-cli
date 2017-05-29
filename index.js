4570059f3b13e3e446be23042752c79e3231b352
4lk4tr43 <4lk4tr43@gmail.com>
2017-05-29 15:54:29 +0200

    Changed paths to src folder

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const style = require('./helpers/style');
+const style = require('./src/helpers/style');
 
 const command = process.argv[2];
 const args = process.argv.slice(3);
 
 try {
-    require(__dirname + '/commands/' + command)[process.argv.indexOf('help') > -1 ? 'help' : 'run'](args);
+    require(__dirname + '/src/commands/' + command)[process.argv.indexOf('help') > -1 ? 'help' : 'run'](args);
 } catch (e) {
     if (/Error:\sCannot\sfind\smodule/.test(e)) {
         console.error(style.error('Error: command not found. Available commands are:'));
