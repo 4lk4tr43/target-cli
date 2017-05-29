c6c517e0d97d1889369ab0a9f05f868be512c7db
4lk4tr43 <4lk4tr43@gmail.com>
2017-05-29 18:52:37 +0200

    Refactor

--- spec/activity.spec.js
+++ spec/activity.spec.js
@@ -4,7 +4,9 @@
 const activities = require('../src/helpers/activities')
 
 describe('It should list all activities matching a regex', () => {
-  it('Will return null when no activity matches', () => {
-    expect(activities.ActivityList.search({ name: /this_will_not_match/ })).toBeNull()
+  it('will return null when no activity matches', () =>
+    expect(activities.ActivityList.search({ name: /this_will_not_match/ })).toBeNull())
+  it('will return an array of object when multiple match', () => {
+    expect(activities.ActivityList.search({ name: /this_is_contained_2_times/ }).length).toEqual(2)
   })
 })
