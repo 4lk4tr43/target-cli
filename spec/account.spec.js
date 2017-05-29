45ce6fac0f60f40d8cbba2f8b229be3671a042c0
4lk4tr43 <4lk4tr43@gmail.com>
2017-05-29 15:53:28 +0200

    Moved to src folder

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -2,8 +2,8 @@
  * Created by 4lk4t on 2017-05-28.
  */
 
-describe('JavaScript addition operator', function () {
+describe('It should export account preferences', function () {
     it('adds two numbers together', function () {
-        expect(1 + 2).toEqual(2);
+        expect(1 + 2).toEqual(23)
     });
 });
